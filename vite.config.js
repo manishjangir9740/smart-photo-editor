51d1f2ed69d17794fca57366738c14e684f42d8d
manishjangir9740 <150588418+manishjangir9740@users.noreply.github.com>
2025-07-15 12:47:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
@@ -11,27 +11,16 @@ export default defineConfig({
     terserOptions: {
       compress: {
         drop_console: true,
-        drop_debugger: true
-      }
+        drop_debugger: true,
+      },
     },
     rollupOptions: {
       output: {
         manualChunks: {
           'react-vendor': ['react', 'react-dom'],
-          'mui-vendor': ['@mui/material', '@mui/icons-material']
-        }
-      }
-    }
+          'mui-vendor': ['@mui/material', '@mui/icons-material'],
+        },
+      },
+    },
   },
-  server: {
-   allowedHosts: ['smart-photo-editor.onrender.com']
-   port: process.env.PORT || 3000,
-  host: true,
-  strictPort: true
-  },
-  preview: {
-    port: process.env.PORT || 3000,
-    host: true,
-    strictPort: true
-  }
 })
